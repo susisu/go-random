e220fc37fc9a2a7ef46f3952f9dc51ab7842d305
Susisu <susisu2413@yahoo.co.jp>
2023-03-09 01:25:25 +0900

    docs: add comments to Generator

--- uint32/generator.go
+++ uint32/generator.go
@@ -1,5 +1,6 @@
 package random
 
+// Generator is an abstract random number generator that yields uint32 values.
 type Generator interface {
 	Uint32() uint32
 }
--- uint64/generator.go
+++ uint64/generator.go
@@ -2,6 +2,7 @@ package random
 
 import "math/rand"
 
+// Generator is an abstract random number generator that yields uint64 values.
 type Generator interface {
 	Uint64() uint64
 }
