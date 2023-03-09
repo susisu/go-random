a9493db18254d9c65fd566f04dc3e7873b4633f5
Susisu <susisu2413@yahoo.co.jp>
2023-03-10 02:14:01 +0900

    docs: improve comments

--- uint64/random.go
+++ uint64/random.go
@@ -5,37 +5,37 @@ import (
 	"math/bits"
 )
 
-// Int returns a random int value in the interval [-2^n, 2^n-1].
+// Int returns a random int value within the range [-2^n, 2^n-1].
 func Int(g Generator) int {
 	return int(g.Uint64())
 }
 
-// Int32 returns a random int32 value in the interval [-2^31, 2^31-1].
+// Int32 returns a random int32 value within the range [-2^31, 2^31-1].
 func Int32(g Generator) int32 {
 	return int32(g.Uint64())
 }
 
-// Int64 returns a random int64 value in the interval [-2^63, 2^63-1].
+// Int64 returns a random int64 value within the range [-2^63, 2^63-1].
 func Int64(g Generator) int64 {
 	return int64(g.Uint64())
 }
 
-// Uint returns a random uint value in the interval [0, 2^n-1].
+// Uint returns a random uint value within the range [0, 2^n-1].
 func Uint(g Generator) uint {
 	return uint(g.Uint64())
 }
 
-// Uint32 returns a random uint32 value in the interval [0, 2^32-1].
+// Uint32 returns a random uint32 value within the range [0, 2^32-1].
 func Uint32(g Generator) uint32 {
 	return uint32(g.Uint64())
 }
 
-// Uint64 returns a random uint64 value in the interval [0, 2^64-1].
+// Uint64 returns a random uint64 value within the range [0, 2^64-1].
 func Uint64(g Generator) uint64 {
 	return g.Uint64()
 }
 
-// uintAtMost returns a random uint value in the interval [0, max].
+// uintAtMost returns a random uint value within the range [0, max].
 func uintAtMost(g Generator, max uint) uint {
 	if max == uint(math.MaxUint) {
 		return Uint(g)
@@ -52,7 +52,7 @@ func uintAtMost(g Generator, max uint) uint {
 	}
 }
 
-// uint32AtMost returns a random uint32 value in the interval [0, max].
+// uint32AtMost returns a random uint32 value within the range [0, max].
 func uint32AtMost(g Generator, max uint32) uint32 {
 	if max == uint32(math.MaxUint32) {
 		return Uint32(g)
@@ -69,7 +69,7 @@ func uint32AtMost(g Generator, max uint32) uint32 {
 	}
 }
 
-// uint64AtMost returns a random uint64 value in the interval [0, max].
+// uint64AtMost returns a random uint64 value within the range [0, max].
 func uint64AtMost(g Generator, max uint64) uint64 {
 	if max == uint64(math.MaxUint64) {
 		return Uint64(g)
@@ -86,7 +86,7 @@ func uint64AtMost(g Generator, max uint64) uint64 {
 	}
 }
 
-// IntBetween returns a random int value in the interval [min, max].
+// IntBetween returns a random int value within the range [min, max].
 // It panics if min > max is given.
 func IntBetween(g Generator, min, max int) int {
 	if min == max {
@@ -94,11 +94,11 @@ func IntBetween(g Generator, min, max int) int {
 	} else if min < max {
 		return int(uintAtMost(g, uint(max)-uint(min))) + min
 	} else {
-		panic("invalid argument to IntBetween: min > max")
+		panic("invalid argument to IntBetween: min must be less than or equal to max")
 	}
 }
 
-// Int32Between returns a random int32 value in the interval [min, max].
+// Int32Between returns a random int32 value within the range [min, max].
 // It panics if min > max is given.
 func Int32Between(g Generator, min, max int32) int32 {
 	if min == max {
@@ -106,11 +106,11 @@ func Int32Between(g Generator, min, max int32) int32 {
 	} else if min < max {
 		return int32(uint32AtMost(g, uint32(max)-uint32(min))) + min
 	} else {
-		panic("invalid argument to Int32Between: min > max")
+		panic("invalid argument to Int32Between: min must be less than or equal to max")
 	}
 }
 
-// Int64Between returns a random int64 value in the interval [min, max].
+// Int64Between returns a random int64 value within the range [min, max].
 // It panics if min > max is given.
 func Int64Between(g Generator, min, max int64) int64 {
 	if min == max {
@@ -118,11 +118,11 @@ func Int64Between(g Generator, min, max int64) int64 {
 	} else if min < max {
 		return int64(uint64AtMost(g, uint64(max)-uint64(min))) + min
 	} else {
-		panic("invalid argument to Int64Between: min > max")
+		panic("invalid argument to Int64Between: min must be less than or equal to max")
 	}
 }
 
-// UintBetween returns a random uint value in the interval [min, max].
+// UintBetween returns a random uint value within the range [min, max].
 // It panics if min > max is given.
 func UintBetween(g Generator, min, max uint) uint {
 	if min == max {
@@ -130,11 +130,11 @@ func UintBetween(g Generator, min, max uint) uint {
 	} else if min < max {
 		return uintAtMost(g, max-min) + min
 	} else {
-		panic("invalid argument to UintBetween: min > max")
+		panic("invalid argument to UintBetween: min must be less than or equal to max")
 	}
 }
 
-// Uint32Between returns a random uint32 value in the interval [min, max].
+// Uint32Between returns a random uint32 value within the range [min, max].
 // It panics if min > max is given.
 func Uint32Between(g Generator, min, max uint32) uint32 {
 	if min == max {
@@ -142,11 +142,11 @@ func Uint32Between(g Generator, min, max uint32) uint32 {
 	} else if min < max {
 		return uint32AtMost(g, max-min) + min
 	} else {
-		panic("invalid argument to Uint32Between: min > max")
+		panic("invalid argument to Uint32Between: min must be less than or equal to max")
 	}
 }
 
-// Uint64Between returns a random uint64 value in the interval [min, max].
+// Uint64Between returns a random uint64 value within the range [min, max].
 // It panics if min > max is given.
 func Uint64Between(g Generator, min, max uint64) uint64 {
 	if min == max {
@@ -154,16 +154,16 @@ func Uint64Between(g Generator, min, max uint64) uint64 {
 	} else if min < max {
 		return uint64AtMost(g, max-min) + min
 	} else {
-		panic("invalid argument to Uint64Between: min > max")
+		panic("invalid argument to Uint64Between: min must be less than or equal to max")
 	}
 }
 
-// Float32 returns a random float32 value in the interval [0, 1).
+// Float32 returns a random float32 value within the range [0, 1).
 func Float32(g Generator) float32 {
 	return float32(uint32AtMost(g, (1<<24)-1)) / (1 << 24)
 }
 
-// Float64 returns a random float64 value in the interval [0, 1).
+// Float64 returns a random float64 value within the range [0, 1).
 func Float64(g Generator) float64 {
 	return float64(uint64AtMost(g, (1<<53)-1)) / (1 << 53)
 }
